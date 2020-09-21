faa0969998c4a6c976b110a274e4ce442b22fbee
penglingyu <penglingyu@csmadik.com>
2020-09-21 16:00:30 +0800

    系统配置初步修改

---
+++ Controllers/SystemController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeanMvcProject.Controllers
+{
+    public class SystemController : Controller
+    {
+        public new IActionResult User()
+        {
+            return View();
+        }
+
+        public IActionResult Role()
+        {
+            return View();
+        }
+
+        public IActionResult Deptment()
+        {
+            return View();
+        }
+
+        public IActionResult Power()
+        {
+            return View();
+        }
+
+        public IActionResult logging()
+        {
+            return View();
+        }
+    }
+}
